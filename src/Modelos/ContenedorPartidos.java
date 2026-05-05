868ea5eda1892b1425f2a593cf564fb8007b9b09
jorge <jorge@JMB>
2026-05-05 15:22:14 +0200

    Actualizacion de ContenedorPartidos y Partido

--- src/Modelos/ContenedorPartidos.java
+++ src/Modelos/ContenedorPartidos.java
@@ -4,10 +4,90 @@
  */
 package Modelos;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  *
  * @author jorge
  */
 public class ContenedorPartidos {
-    
+
+    // Atributos 
+    private ArrayList<Partido> listaPartidos;
+    private HashMap<String, Partido> mapaPartidos;
+    private HashSet<Integer> temporadasRegistradas;
+
+    // Constructor
+    public ContenedorPartidos(ArrayList<Partido> listaPartidos, HashMap<String, Partido> mapaPartidos, HashSet<Integer> temporadasRegistradas) {
+        this.listaPartidos = listaPartidos;
+        this.mapaPartidos = mapaPartidos;
+        this.temporadasRegistradas = temporadasRegistradas;
+    }
+
+    // Metodos
+    // Genera una clave para poder identificar el partido 
+    private String generarClave(int codigoLocal, int codigoVisitante, int anioTemporada) {
+        return codigoLocal + "-" + codigoVisitante + "-" + anioTemporada;
+    }
+
+    // Añade los partidos a los distintos contenedores
+    public void añadirPartido(Partido partido) {
+        listaPartidos.add(partido);
+        String clave = generarClave(partido.getCodigoEquipoLocal(),
+                partido.getCodigoEquipoVisitante(),
+                partido.getAñoTemporada());
+        mapaPartidos.put(clave, partido);
+        temporadasRegistradas.add(partido.getAñoTemporada());
+    }
+
+    // Genera una clave para poder identificar los partidos
+    public Partido getPartido(int codigoLocal, int codigoVisitante, int anioTemporada) {
+        return mapaPartidos.get(generarClave(codigoLocal, codigoVisitante, anioTemporada));
+    }
+
+    // Te devuelve la lista de partidos que has insertado durante la sesion
+    public ArrayList<Partido> getListaPartidos() {
+        return listaPartidos;
+    }
+
+    // Obtiene el mapa de todos los partidos
+    public HashMap<String, Partido> getMapaPartidos() {
+        return mapaPartidos;
+    }
+
+    // Obtiene el conjunto de temporadas distitnas 
+    public HashSet<Integer> getTemporadasRegistradas() {
+        return temporadasRegistradas;
+    }
+
+    // Te devuelve el numero de partidos que hay en el contenedor
+    public int getTamaño() {
+        return listaPartidos.size();
+    }
+
+    // Verifica si el contenedor esta vacio 
+    public boolean estaVacio() {
+        return listaPartidos.isEmpty();
+    }
+
+    // Borra los partidos de todos las colecciones y del contenedor
+    public void limpiar() {
+        listaPartidos.clear();
+        mapaPartidos.clear();
+        temporadasRegistradas.clear();
+    }
+
+    // Muestra los partidos insertados durante la sesion
+    public void mostrarPartidosSesion() {
+        if (listaPartidos.isEmpty()) {
+            System.out.println("No se han insertado partidos durante esta sesión.");
+        } else {
+            System.out.println("Partidos insertados durante esta sesión (" + listaPartidos.size() + "):");
+            for (Partido p : listaPartidos) {
+                System.out.println("  " + p.toString());
+            }
+        }
+    }
 }
--- src/Modelos/Partido.java
+++ src/Modelos/Partido.java
@@ -4,30 +4,96 @@
  */
 package Modelos;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 /**
  *
  * @author jorge
  */
-public class Partido {
-    // Atributos
+public class Partido implements Serializable{
 
+    // Atributos
     protected int codigoEquipoLocal;
     protected int codigoEquipoVisitante;
     protected int añoTemporada;
     protected String fecha;
-    protected int partidoTitular;
+    protected Integer puntuacionLocal;
+    protected Integer puntuacionVisitante;
 
     // Constructor
-    public Partido(int codigoEquipoLocal, int codigoEquipoVisitante, int añoTemporada, String fecha, int partidoTitular) {
+    public Partido(int codigoEquipoLocal, int codigoEquipoVisitante, int añoTemporada, String fecha, Integer puntuacionLocal, Integer puntuacionVisitante) {
         this.codigoEquipoLocal = codigoEquipoLocal;
         this.codigoEquipoVisitante = codigoEquipoVisitante;
         this.añoTemporada = añoTemporada;
         this.fecha = fecha;
-        this.partidoTitular = partidoTitular;
+        this.puntuacionLocal = puntuacionLocal;
+        this.puntuacionVisitante = puntuacionVisitante;
+    }
+
+    public Partido() {
+    }
+
+    // Metodos
+    public int getCodigoEquipoLocal() {
+        return codigoEquipoLocal;
+    }
+
+    public void setCodigoEquipoLocal(int codigoEquipoLocal) {
+        this.codigoEquipoLocal = codigoEquipoLocal;
+    }
+
+    public int getCodigoEquipoVisitante() {
+        return codigoEquipoVisitante;
+    }
+
+    public void setCodigoEquipoVisitante(int codigoEquipoVisitante) {
+        this.codigoEquipoVisitante = codigoEquipoVisitante;
+    }
+
+    public int getAñoTemporada() {
+        return añoTemporada;
+    }
+
+    public void setAnioTemporada(int añoTemporada) {
+        this.añoTemporada = añoTemporada;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getPuntuacionLocal() {
+        return puntuacionLocal;
+    }
+
+    public void setPuntuacionLocal(Integer puntuacionLocal) {
+        this.puntuacionLocal = puntuacionLocal;
+    }
+
+    public Integer getPuntuacionVisitante() {
+        return puntuacionVisitante;
+    }
+
+    public void setPuntuacionVisitante(Integer puntuacionVisitante) {
+        this.puntuacionVisitante = puntuacionVisitante;
+    }
+
+    // Devuelve los datos del partido 
+    @Override
+    public String toString() {
+        return "Partido{" + "codigoEquipoLocal=" + codigoEquipoLocal +
+                ", codigoEquipoVisitante=" + codigoEquipoVisitante + ", "
+                + "a\u00f1oTemporada=" + añoTemporada + ", "
+                + "fecha=" + fecha + ", "
+                + "puntuacionLocal=" + puntuacionLocal + ", "
+                + "puntuacionVisitante=" + puntuacionVisitante + '}';
     }
     
-    // Metodos
+    
 
 }
