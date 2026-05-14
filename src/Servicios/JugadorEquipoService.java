dc83efc7b6dd7095f5b4e2e24903cf658b1d2ec1
isard <isard@DESKTOP-P0UA02U>
2026-05-14 18:13:02 +0200

    Actualización de jugadorEquipoService

--- src/Servicios/JugadorEquipoService.java
+++ src/Servicios/JugadorEquipoService.java
@@ -5,8 +5,11 @@
 package Servicios;
 import Excepciones.ElDatoIntroducidoEsIncorrecto;
 import Excepciones.SeHaProducidoUnError;
+import Excepciones.YaImportadoException;
 import Interfaces.MetodosComunes;
 import Modelos.Jugador_equipo;
+import Utils.Constantes;
+import com.google.gson.reflect.TypeToken;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +21,7 @@ import java.util.List;
  */
 public class JugadorEquipoService implements MetodosComunes<Jugador_equipo>{
     
-   // Implementar el crud de Jugadorequipo
+   // Implementar el crud de JugadorEquipo
     
     //Atributos
     //Hace de memoria para poder almacenar los equipos introducidos durante la sesión.
@@ -151,7 +154,154 @@ public class JugadorEquipoService implements MetodosComunes<Jugador_equipo>{
         }
         return lista;
     }
+    
+    //EXPORTACIONES
 
+    /**
+     * Metodos de exportaciona TXT de JugadorEquipo
+     * @throws SeHaProducidoUnError 
+     */
+    public void exportarTXT() throws SeHaProducidoUnError {
+        List<Jugador_equipo>lista= consultarTodos();
+        List<String>lineas=new ArrayList<>();
+        
+        for (Jugador_equipo je : lista) {
+            lineas.add(je.getCodigoEquipo() + Constantes.SEPARADOR_TXT 
+                    + je.getCodigoJugador() + Constantes.SEPARADOR_TXT 
+                    + je.getAñoEntrada() + Constantes.SEPARADOR_TXT 
+                    + (je.getAñoSalida() != null ? je.getAñoSalida(): "null") + Constantes.SEPARADOR_TXT + (je.getPartidosTitular()!=null ? je.getPartidosTitular():"null"));
+        }
+        
+        MetodosFicheros.exportarTxt(Constantes.FICHERO_JUGADOR_EQUIPO, lineas);
+    }
+    
+    /**
+     * Metodos de exportaciona CSv de JugadorEquipo
+     * @throws SeHaProducidoUnError 
+     */
+    public void exportarCSv() throws SeHaProducidoUnError {
+        List<Jugador_equipo>lista = consultarTodos();
+        List<String>lineas= new ArrayList<>();
+        for (Jugador_equipo je : lista) {
+            lineas.add(je.getCodigoEquipo() + Constantes.SEPARADOR_CSV 
+                    + je.getCodigoJugador() + Constantes.SEPARADOR_CSV 
+                    + je.getAñoEntrada() + Constantes.SEPARADOR_CSV 
+                    + (je.getAñoSalida() != null ? je.getAñoSalida():"null") 
+                    + Constantes.SEPARADOR_CSV + (je.getPartidosTitular()!=null ? je.getPartidosTitular():"null"));
+        }
+        MetodosFicheros.exportarCsv(Constantes.FICHERO_JUGADOR_EQUIPO, lineas);
+    }
+    
+    /**
+     * Metodos de exportaciona Binario de JugadorEquipo
+     * @throws SeHaProducidoUnError 
+     */
+    public void exportarBinario() throws SeHaProducidoUnError {
+        MetodosFicheros.exportarBinario(Constantes.FICHERO_JUGADOR_EQUIPO, consultarTodos());
+    }
+    
+    
+    /**
+     * Metodos de exportaciona Json de JugadorEquipo
+     * @throws SeHaProducidoUnError 
+     */
+    public void exportarJson() throws SeHaProducidoUnError {
+        MetodosFicheros.exportarJson(Constantes.FICHERO_JUGADOR_EQUIPO, consultarTodos());
+    }
+    
+    
+    //IMPORTACION
+    
+    /**
+     * Metodos de importacion Txt de JugadorEquipo
+     * @throws SeHaProducidoUnError 
+     */
+    public void importarTxt() throws SeHaProducidoUnError, YaImportadoException, ElDatoIntroducidoEsIncorrecto {
+        List<String>lineas=MetodosFicheros.importarTxt(Constantes.FICHERO_JUGADOR_EQUIPO, txtImportado);
+        for (String linea : lineas) {
+            insertar(parsear(linea.split(Constantes.SEPARADOR_TXT)));
+        }
+        txtImportado=true;
+    }
+    
+    /**
+     * Metodos de importacion Csv de JugadorEquipo
+     * @throws SeHaProducidoUnError 
+     */
+    public void importarCsv() throws SeHaProducidoUnError, YaImportadoException, ElDatoIntroducidoEsIncorrecto {
+        List<String>lineas=MetodosFicheros.importarCsv(Constantes.FICHERO_JUGADOR_EQUIPO, csvImportado);
+        
+        for (String linea : lineas) {
+            insertar(parsear(linea.split(Constantes.SEPARADOR_CSV)));
+        }
+        csvImportado=true;
+    }
+    
+    /**
+     * Metodos de importacion Binario de JugadorEquipo
+     * @throws SeHaProducidoUnError 
+     */
+    public void importarBinario() throws SeHaProducidoUnError, YaImportadoException, ElDatoIntroducidoEsIncorrecto {
+        List<Jugador_equipo>lista=MetodosFicheros.exportarBinario(Constantes.FICHERO_JUGADOR_EQUIPO, binImportado);
+        
+        for (Jugador_equipo je : lista) {
+            insertar(je);
+        }
+        binImportado=true;
+    }
+    
+    /**
+     * Metodos de importacion Json de JugadorEquipo
+     * @throws SeHaProducidoUnError 
+     */
+    public void importarJson() throws SeHaProducidoUnError, YaImportadoException, ElDatoIntroducidoEsIncorrecto {
+        List<Jugador_equipo>lista=MetodosFicheros.importarJson(Constantes.FICHERO_JUGADOR_EQUIPO, jsonImportado, 
+                new TypeToken<List<Jugador_equipo>>(){}.getType());
+        
+        for (Jugador_equipo je : lista) {
+            insertar(je);
+        }
+        jsonImportado=true;
+    }
+    
+    
+    //METODOS AUXILIARES
+    
+    /**
+     * Mapea una fila mediante el REesultSet a un objeto en este caso de JugadorEquipo
+     * @param rs
+     * @return
+     * @throws SQLException 
+     */
+    private Jugador_equipo mapear(ResultSet rs) throws SQLException {
+        Integer añoSalida = rs.getObject("año_salida") !=null ? rs.getInt("año_salida"):null;
+        Integer partidosTitular = rs.getObject("partidos_titular") !=null ? rs.getInt("partidos_titular"):null;
+        
+        return new Jugador_equipo(rs.getInt("codigo_equipo"),rs.getInt("codigo_jugador"),rs.getInt("año_entrada"),añoSalida,partidosTitular);
+    }
+    
+    
+    private Jugador_equipo parsear(String[]campos) throws ElDatoIntroducidoEsIncorrecto{
+        if (campos.length < 5) {
+            throw new ElDatoIntroducidoEsIncorrecto("Formato de linea incorrecto");
+        }
+        
+        Integer añoSalida=campos
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 }
